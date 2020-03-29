be0c3830fd4562b3e7565094d3ef6f6ac187f222
Michał Kubiak <michal51112@gmail.com>
2020-03-29 11:12:35 +0200

    updated angular packages.

--- src/main/webapp/src/app/module/material/material.module.ts
+++ src/main/webapp/src/app/module/material/material.module.ts
@@ -2,16 +2,20 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatButtonModule} from '@angular/material/button';
-import {
-  MatCardModule, MatDividerModule,
-  MatFormFieldModule, MatGridListModule,
-  MatInputModule, MatListModule, MatPaginatorModule, MatRadioModule,
-  MatSelectModule,
-  MatSlideToggleModule,
-  MatStepperModule, MatTreeModule,
-  MatExpansionModule,
-  MatTableModule
-} from "@angular/material";
+import { MatCardModule } from "@angular/material/card";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatTableModule } from "@angular/material/table";
+import { MatTreeModule } from "@angular/material/tree";
 import {MatNativeDateModule} from "@angular/material/core";
 
 @NgModule({
